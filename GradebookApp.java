9a9174e621b3254dec71379594962e5bcdd756be
Isaac Gallegos <Isaac Gallegos>
2016-05-12 13:35:07 -0600

    'first_change'

--- GradebookApp.java
+++ GradebookApp.java
@@ -3,7 +3,12 @@
 class GradebookApp {
 	
 	public static void main(String[] args) {
-
+		String name_1 = "Isaac";
+		Student student_1 =  new Student(name_1);
+		System.out.println("High: " + student_1.highest_grade());
+		System.out.println("Low: " + student_1.lowest_grade());
+		System.out.println("Average: " + student_1.average_grade());
+		System.out.println("Letter Grade: " + Student.letter_grade(student_1.average_grade()));
 	}
 
 }
--- Student.java
+++ Student.java
@@ -1,5 +1,60 @@
 // Student.java v.20160502
-
+import java.util.Random;
 class Student {
+	String name;
+	double[] grades = new double[10];
 
+	public Student(String n) {
+		name = n;
+		for (int i = 0; i < 10; i++){
+			grades[i] = (Math.random() * 100) + 1;
+			System.out.println(grades[i]);
+		}
+	}
+
+	public double highest_grade() {
+		double maximum = grades[0];
+
+        for(int i = 0; i < 10; i++) {
+            if (grades[i] > maximum) {
+                maximum = grades[i];
+            }
+        }
+        return maximum;
+	}
+
+	public double lowest_grade() {
+		double minimum = grades[0];
+
+        for(int i = 0; i < 10; i++) {
+            if (grades[i] < minimum) {
+                minimum = grades[i];
+            }
+        }
+		return minimum;
+	}
+
+	public double average_grade() {
+		double sum = 0;        
+        for(int i = 0; i < 10; i++) {
+            sum = sum + grades[i];
+        }
+        double average = (sum / 10);
+		return average;
+	}
+
+	static char letter_grade(double average_grade) {
+		char let_grade = ' ';
+		if (average_grade >= 90 && average_grade <= 100)
+			let_grade = 'A';
+		if (average_grade >= 80 && average_grade <= 89.99)
+			let_grade = 'B';
+		if (average_grade >= 70 && average_grade <= 79.99)
+			let_grade = 'C';
+		if (average_grade >= 60 && average_grade <= 69.99)
+			let_grade = 'D';
+		else
+			let_grade = 'F';
+		return let_grade;
+	}
 }
